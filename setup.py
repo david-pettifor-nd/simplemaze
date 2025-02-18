301eb77dd77ef5da853bf05457db3a66c5acc392
David Pettifor <dpettifo@nd.edu>
2025-02-18 12:25:26 -0500

    release v0.1.1 with MIT license

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="simplemaze",
-    version="0.1.0",
+    version="0.1.1",
     description="A Python library for generating mazes",
     long_description=README,
     long_description_content_type="text/markdown",
